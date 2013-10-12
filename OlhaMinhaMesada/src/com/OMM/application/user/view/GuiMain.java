67ef052e0c8d37592a500dfeab32313b9cd49a0a
ViBorges <vbda123@gmail.com>
2013-10-12 04:03:43 -0300

    Implementação da função de pesquisa

--- OlhaMinhaMesada/src/com/OMM/application/user/view/GuiMain.java
+++ OlhaMinhaMesada/src/com/OMM/application/user/view/GuiMain.java
@@ -1,11 +1,16 @@
 package com.OMM.application.user.view;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.SearchView;
 
 public class GuiMain extends Activity {
 	
@@ -14,6 +19,8 @@ public class GuiMain extends Activity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gui_main);
+		
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		Button btn = (Button)findViewById(R.id.button1);
 		final MediaPlayer mp = MediaPlayer.create(this, R.raw.click);
@@ -42,11 +49,44 @@ public class GuiMain extends Activity {
             		    GuiMain.this.finish();
                     }
                 });
+        
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
+        
+	}
+        
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.options_menu, menu);
+		
+		SearchManager searchManager =
+		           (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		    SearchView searchView =
+		            (SearchView) menu.findItem(R.id.search).getActionView();
+		    searchView.setSearchableInfo(
+		            searchManager.getSearchableInfo(getComponentName()));
+
+		    return true;
 		
 	}
-	
-	
-	
-	
 
 }
+	
+
+
--- OlhaMinhaMesada/src/com/OMM/application/user/view/SplashScreen.java
+++ OlhaMinhaMesada/src/com/OMM/application/user/view/SplashScreen.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 
 
 public class SplashScreen extends Activity {
@@ -37,12 +36,5 @@ public class SplashScreen extends Activity {
 		
 		}
 		
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash_screen, menu);
-		return true;
-	}
 
 }
