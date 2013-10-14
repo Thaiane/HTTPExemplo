505f8a98c90928d5d08a26d4bd47a3f5be3caf78
Rudmar <rudmar_junior@hotmail.com>
2013-10-13 22:42:39 -0300

    Implementacao SocketUser

---
+++ OlhaMinhaMesada/src/com/OMM/application/user/dao/SocketUser.java
@@ -0,0 +1,46 @@
+package com.OMM.application.user.dao;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketUser {
+
+	Socket		socketUsuario;
+	Scanner		entrada;
+	PrintWriter saida;
+	
+	public SocketUser( ) {
+		
+		configurarRede( );
+	}
+	
+	private void configurarRede( ) {
+		
+		try {
+			
+			socketUsuario = new Socket( "192.168.1.11", 5000 );
+			saida		  = new PrintWriter( socketUsuario.getOutputStream( ) );
+			entrada		  = new Scanner( socketUsuario.getInputStream( ) );
+			
+		} catch( Exception e ) {
+			
+		}
+	}
+	
+	public Scanner getEntrada() {
+		return entrada;
+	}
+
+	public void setEntrada(Scanner entrada) {
+		this.entrada = entrada;
+	}
+
+	public PrintWriter getSaida() {
+		return saida;
+	}
+
+	public void setSaida(PrintWriter saida) {
+		this.saida = saida;
+	}
+}
